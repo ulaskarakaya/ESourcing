b8862c7da5fad79e7450f3be1468f655c8c3b265
ulaş karakaya <ulas_gs_11@hotmail.com>
2022-09-04 23:33:08 +0300

    Add product class In ProductServices

---
+++ ESourcing.Products/Controllers/ProductController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ESourcing.Products.Controllers
+{
+    [Route("api/v1/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        
+    }
+}

786e9060b049946d2cc2e48ed1257b9956414651
Ulaş Karakaya <Ulas.Karakaya@gelecekvarlik.com.tr>
2023-03-16 10:07:39 +0300

    AddedSourcingOperations

---
+++ ESourcing.Sourcing/Repositories/Interfaces/IAuctionRepository.cs
@@ -0,0 +1,16 @@
+﻿using ESourcing.Sourcing.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ESourcing.Sourcing.Repositories.Interfaces
+{
+    public interface IAuctionRepository
+    {
+        Task<IEnumerable<Auction>> GetAuctions();
+        Task<Auction> GetAuction(string id);
+        Task<Auction> GetAuctionByName(string name);
+        Task Create(Auction auction);
+        Task<bool> Update(Auction auction);
+        Task<bool> Delete(string id);
+    }
+}
